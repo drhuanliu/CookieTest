3cf1d41e7a54576457ec589eccbb3b45b277e083
Huan Liu <huanliu@gmail.com>
2021-10-01 04:17:38 +0000

    browser SFSafariViewController test

--- BrowserTest/ViewController.swift
+++ BrowserTest/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    
+    @IBAction func showClicked(_ sender: Any) {
+        show()
+    }
+    func show () {
+        let urlString = "https://drhuanliu.github.io/index.html"
+
+        if let url = URL(string: urlString) {
+            let vc = SFSafariViewController(url: url, entersReaderIfAvailable: true)
+            vc.delegate = self
+
+            present(vc, animated: true)
+        }
     }
 
 
